7c0fd7ba588c30704f5a064ff732f6749dad352b
PANHARO <panharokeo097@gmail.com>
2025-12-20 13:36:39 +0700

    .

--- json_lesson/index.js
+++ json_lesson/index.js
@@ -15,27 +15,70 @@
 // 3. Take out the trash
 
 // 1. Wash Dishes
-function WashDishes(){
-    console.log("I have washed dishes!")
-}
-setTimeout(()=>{
-    WashDishes()
-}, 2000)
+// function WashDishes(){
+//     console.log("I have washed dishes!")
+// }
+// setTimeout(()=>{
+//     WashDishes()
+// }, 2000)
+// promise is process which pending to wait for (fulfill or reject)
+const WashDishes = new Promise((resolve, reject)=>{
+    let isTrue = false;
+    if (isTrue){
+        setTimeout(()=>{
+        resolve("You have washed the dishes!")
+    },2000);
+    }else{
+        reject("You haven't washed the dishes!")
+    }
+})
 // 2. Cook Food 
-function CookFood(){
-    console.log("I have cooked the food!")
-}
-setTimeout(()=>{
-    CookFood()
-},5000)
+// function CookFood(){
+//     console.log("I have cooked the food!")
+// }
+// setTimeout(()=>{
+//     CookFood()
+// },5000)
+const cookFood = new Promise((resolve, reject)=>{
+    let isTrue = true;
+    if (isTrue){
+        setTimeout(()=>{
+        resolve("You have cooked the food!")
+    },2000);
+    }else{
+        reject("You haven't cooked the food!")
+    }
+})
 
 //3. Take out the trash
-function TakeOutTheTrash(){
-    console.log("I took out the trash!")
-}
-setTimeout(()=>{
-    TakeOutTheTrash()
-},1000)
+// function TakeOutTheTrash(){
+//     console.log("I took out the trash!")
+// }
+// setTimeout(()=>{
+//     TakeOutTheTrash()
+// },1000)
+const TakeOutTheTrash = new Promise((resolve, reject)=>{
+    let isTrue = true;
+    if (isTrue){
+        setTimeout(()=>{
+        resolve("You have taken out the trashes!")
+    },2000);
+    }else{
+        reject("You haven't taken out the trashes!")
+    }
+})
+
+// calling function which implement with promises
+WashDishes.then((task)=> {
+    console.log(task); return cookFood;
+})
+        .then((task)=>{
+            console.log(task); return TakeOutTheTrash;
+        })
+        .then((task)=>{
+             console.log(task); return console.log("You have completed all task");
+        })
+        .catch((error)=>console.log(error))
 
 // calling each function
 // WashDishes();
